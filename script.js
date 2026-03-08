fdb985470f292785269b66ec60ea39fc0570f559
gerilyansyah4-debug <gerilyansyah4@gmail.com>
2026-03-08 19:50:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
 window.addEventListener("click",()=>{
-
 document.getElementById("music").play()
-
 })
 
 const rain=document.getElementById("rain")
 
 for(let i=0;i<150;i++){
-
 let heart=document.createElement("div")
-
 heart.className="heart"
-
 heart.innerHTML="💗"
-
 heart.style.left=Math.random()*100+"vw"
-
 heart.style.animationDuration=(Math.random()*3+2)+"s"
-
 heart.style.fontSize=(Math.random()*20+10)+"px"
-
 rain.appendChild(heart)
-
 }
 
-
 let count=3
-
 let countdown=document.getElementById("countdown")
 
 let timer=setInterval(()=>{
-
 count--
-
 countdown.innerText=count
 
 if(count==0){
@@ -44,15 +30,11 @@ countdown.style.display="none"
 document.getElementById("title").classList.remove("hidden")
 
 setTimeout(()=>{
-
 document.getElementById("bigHeart").classList.remove("hidden")
-
 },2000)
 
 setTimeout(()=>{
-
 document.getElementById("ready").classList.remove("hidden")
-
 },4000)
 
 }
@@ -68,30 +50,24 @@ document.querySelector(".center").style.display="none"
 document.getElementById("bookContainer").classList.remove("hidden")
 
 const pageFlip=new St.PageFlip(
-
 document.getElementById("book"),
-
 {
-width:520,
-height:650,
+width:500,
+height:600,
 showCover:true
 }
-
 )
 
 pageFlip.loadFromHTML(document.querySelectorAll(".page"))
 
 pageFlip.on("flip",(e)=>{
 
-if(e.data >= 8){
+if(e.data==8){
 
 setTimeout(()=>{
-
 document.getElementById("bookContainer").style.display="none"
-
 showPhotoHeart()
-
-},1200)
+},1000)
 
 }
 
@@ -107,8 +83,6 @@ let container=document.getElementById("photoHeart")
 
 container.classList.remove("hidden")
 
-container.innerHTML=""
-
 let photos=[
 
 "WhatsApp Image 2026-03-07 at 23.38.39.jpeg",
@@ -122,16 +96,18 @@ let photos=[
 
 ]
 
+/* posisi membentuk LOVE */
+
 let positions=[
 
-{top:40,left:150},
-{top:80,left:90},
-{top:80,left:210},
-{top:140,left:60},
-{top:140,left:240},
-{top:210,left:110},
-{top:210,left:190},
-{top:270,left:150}
+{top:0,left:110},
+{top:40,left:40},
+{top:40,left:180},
+{top:90,left:10},
+{top:90,left:210},
+{top:160,left:70},
+{top:160,left:150},
+{top:210,left:110}
 
 ]
 
@@ -141,29 +117,14 @@ let img=document.createElement("img")
 
 img.src=src
 
-img.style.position="absolute"
-
-img.style.width="80px"
-
-img.style.height="80px"
-
-img.style.objectFit="cover"
-
-img.style.borderRadius="12px"
-
 img.style.top=positions[i].top+"px"
-
 img.style.left=positions[i].left+"px"
 
 container.appendChild(img)
 
 })
 
-setTimeout(()=>{
-
 showQuestion()
-
-},2000)
 
 }
 
@@ -172,8 +133,6 @@ showQuestion()
 function showQuestion(){
 
 document.getElementById("finalQuestion").classList.remove("hidden")
-
-document.getElementById("loveText").innerHTML=""
 
 let text="Apakah kamu mau menjadi kekasihku sekali lagi?"
 
@@ -203,8 +162,6 @@ function showTyping(){
 
 document.getElementById("typing").classList.remove("hidden")
 
-document.getElementById("text").innerHTML=""
-
 let text="Aku masih sayang kamu. Jadi... maukah kamu kembali bersamaku? 💗"
 
 let i=0
@@ -224,13 +181,5 @@ setTimeout(typing,70)
 }
 
 typing()
-
-}
-
-
-
-function noAnswer(){
-
-alert("yahhh jangan gitu dong 😭 coba pikir lagi ya 💗")
 
 }
