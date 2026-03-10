dde399c2a356be29d265d9d45127c3fb232f95e5
gerilyansyah4-debug <gerilyansyah4@gmail.com>
2026-03-11 00:24:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ let q = document.getElementById("finalQuestion")
 q.classList.remove("hidden")
 q.style.display="flex"
 
-let text="will you be my girlfriend again??"
+let text="would you like to be my girlfriend again?"
 
 let i=0
 
